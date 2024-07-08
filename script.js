6215fbe4fd06cf20aaeabf81f4e8bd460e71a853
Pedro <166652127+ogpdx@users.noreply.github.com>
2024-07-08 10:22:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,71 @@
 const produto = document.querySelector('.container-produto')
 
-function openProduto() {
-    produto.classList.add('active')
+function openProduto(id) {
+    const produto = document.getElementById(id);
+    produto.classList.add('active');
 }
 
-function closeProduto() {
-    produto.classList.remove('active')
+function closeProduto(id) {
+    const produto = document.getElementById(id);
+    produto.classList.remove('active');
+}
+
+const produtodois = document.querySelector('.container-dois')
+
+function openDois(id) {
+    const produtodois = document.getElementById(id);
+    produtodois.classList.add('active');
+}
+
+function closeDois(id) {
+    const produtodois = document.getElementById(id);
+    produtodois.classList.remove('active');
+}
+
+const produtotres = document.querySelector('container-tres')
+
+function openTres(id) {
+    const produtotres = document.getElementById(id);
+    produtotres.classList.add('active');
+}
+
+function closeTres(id) {
+    const produtotres = document.getElementById(id);
+    produtotres.classList.remove('active');
+}
+
+const produtoquatro = document.querySelector('container-quatro')
+
+function openQuatro(id) {
+    const produtoquatro = document.getElementById(id);
+    produtoquatro.classList.add('active');
+}
+
+function closeQuatro(id) {
+    const produtoquatro = document.getElementById(id);
+    produtoquatro.classList.remove('active');
+}
+
+const produtocinco = document.querySelector('container-cinco')
+
+function openCinco(id) {
+    const produtocinco = document.getElementById(id);
+    produtocinco.classList.add('active');
+}
+
+function closeCinco(id) {
+    const produtocinco = document.getElementById(id);
+    produtocinco.classList.remove('active');
+}
+
+const produtoseis = document.querySelector('container-seis')
+
+function openSeis(id) {
+    const produtoseis = document.getElementById(id);
+    produtoseis.classList.add('active');
+}
+
+function closeSeis(id) {
+    const produtoseis = document.getElementById(id);
+    produtoseis.classList.remove('active');
 }
